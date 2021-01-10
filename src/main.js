a37692047ee51457619ec3f2e1d38e5d3ad97cea
Noor Wachid <wachid1818@gmail.com>
2021-01-10 20:50:16 +0700

    minor detail changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const data = {
-    projects: [
-        {
+    projects: [{
             id: 1,
             name: "RainType",
             description: "Very basic typing test written in JavaScript.",
@@ -10,7 +9,7 @@ const data = {
             id: 2,
             name: "WFileServer",
             description: "Complete rewrite simplified version of HFS.",
-            tags: ["JavaScript"]
+            tags: ["JavaScript", "NodeJS"]
         },
         {
             id: 3,
@@ -38,11 +37,42 @@ const data = {
             name: "SimpleBlog",
             description: "Super simple blog written in PHP with MySQL.",
             url: "https://github.com/NoorWachid/SimpleBlog",
-            tags: ["PHP"]
+            tags: ["PHP", "MySQL"]
         }
     ]
 };
 
-$(document).ready(function() {
+const projectCols = [
+    $('<div class="col-md-4 text-center">'),
+    $('<div class="col-md-4 text-center">'),
+    $('<div class="col-md-4 text-center">'),
+];
+const projectTpl = (name, description, tags) => `
+    <div class="project">
+        <img class="d-block mb-3"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;"
+            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_176ebdea450%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_176ebdea450%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.68333435058594%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            data-holder-rendered="true">
+        <div>
+            <h4>${name}</h4>
+            <p>${description}</p>
+            <div>${tags}</div>
+        </div>
+    </div>
+`;
+
+$(document).ready(function () {
     $("#current-year").text(new Date().getFullYear());
+
+    $("#list").append(projectCols[0]);
+    $("#list").append(projectCols[1]);
+    $("#list").append(projectCols[2]);
+
+    for (let i = 0; i < data.projects.length; ++i) {
+        const it = data.projects[i];
+        projectCols[i % 3].append(
+            projectTpl(it.name, it.description, it.tags.join(", "))
+        );
+    }
 });
