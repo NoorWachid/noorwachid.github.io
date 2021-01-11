47de368a5701638f4c780e15c7050d7358e6b378
Noor Wachid <wachid1818@gmail.com>
2021-01-12 00:38:32 +0700

    spliting logic and data

---
+++ src/dataEn.js
@@ -0,0 +1,95 @@
+const data_EN = {
+    ui: {
+        cv: "CV",
+        cvUrl: "cv.svg",
+        projects: "Projects",
+        skills: "Skills",
+        mainSkills: "Main Skills",
+        otherSkills: "Other Skills",
+        contact: "Contact",
+        email: "E-Mail",
+        phone: "Phone",
+        copyright: "All Rights Reserved",
+    },
+
+    dev: {
+        name: "Noor Wachid",
+        description: "C++ Programmer and Web Developer",
+        phone: '+62895383900152',
+        email: 'noorwachid@yandex.com',
+
+        mainSkills: [{
+                name: "C++",
+                description: "Create high-performance library, CLI and graphics based application",
+            },
+            {
+                name: "JavaScript",
+                description: "Make interactive web user interface",
+            },
+            {
+                name: "PHP",
+                description: "Make dynamic website",
+            }
+        ],
+
+        otherSkills: [
+            "HTML/CSS",
+            "MySQL",
+            "Using Git version control",
+            "Using GNU/Linux based OS",
+            "Speaking/Writing in Indonesian and English",
+        ],
+    },
+
+    tags: ["All", "C++", "JavaScript", "PHP"],
+    tagActive: "All",
+    projects: [{
+            id: 1,
+            name: "RainType",
+            description: "Very basic typing test written in JavaScript",
+            url: "https://noorwachid.github.io/RainType",
+            image: "raintype.png",
+            tags: ["JavaScript"]
+        },
+        {
+            id: 2,
+            name: "WFileServer",
+            description: "Complete rewrite simplified version of HFS",
+            url: "https://github.com/NoorWachid/WFileServer",
+            image: "wfs.png",
+            tags: ["JavaScript", "NodeJS"]
+        },
+        {
+            id: 3,
+            name: "FixQuote",
+            description: "Equalize/Differentiate quotation marks",
+            url: "https://noorwachid.github.io/Jar/FixQuote",
+            image: "fixquote.png",
+            tags: ["JavaScript"]
+        },
+        {
+            id: 4,
+            name: "Sprint",
+            description: "Speed reader: Read article or anything faster than ever",
+            url: "https://noorwachid.github.io/Jar/Sprint",
+            image: "sprint.png",
+            tags: ["JavaScript"]
+        },
+        {
+            id: 5,
+            name: "AnggurEngine",
+            description: "Another 2D C++ Game Engine",
+            url: "https://github.com/NoorWachid/Anggur",
+            image: "anggur.png",
+            tags: ["C++"]
+        },
+        {
+            id: 5,
+            name: "SimpleBlog",
+            description: "Super simple blog written in PHP with MySQL",
+            url: "https://github.com/NoorWachid/SimpleBlog",
+            image: "blank.png",
+            tags: ["PHP", "MySQL"]
+        }
+    ]
+};
--- src/main.js
+++ src/main.js
@@ -1,99 +1,3 @@
-const data_EN = {
-    ui: {
-        cv: "CV",
-        cvUrl: "cv.svg",
-        projects: "Projects",
-        skills: "Skills",
-        mainSkills: "Main Skills",
-        otherSkills: "Other Skills",
-        contact: "Contact",
-        email: "E-Mail",
-        phone: "Phone",
-        copyright: "All Rights Reserved",
-    },
-
-    dev: {
-        name: "Noor Wachid",
-        description: "C++ Programmer and Web Developer",
-        phone: '+62895383900152',
-        email: 'noorwachid@yandex.com',
-
-        mainSkills: [{
-                name: "C++",
-                description: "Create high-performance library, CLI and graphics based application",
-            },
-            {
-                name: "JavaScript",
-                description: "Make interactive web user interface",
-            },
-            {
-                name: "PHP",
-                description: "Make dynamic website",
-            }
-        ],
-
-        otherSkills: [
-            "HTML/CSS",
-            "MySQL",
-            "Using Git version control",
-            "Using GNU/Linux based OS",
-            "Speaking/Writing in Indonesian and English",
-        ],
-    },
-
-    tags: ["All", "C++", "JavaScript", "PHP"],
-    tagActive: "All",
-    projects: [{
-            id: 1,
-            name: "RainType",
-            description: "Very basic typing test written in JavaScript",
-            url: "https://noorwachid.github.io/RainType",
-            image: "raintype.png",
-            tags: ["JavaScript"]
-        },
-        {
-            id: 2,
-            name: "WFileServer",
-            description: "Complete rewrite simplified version of HFS",
-            url: "https://github.com/NoorWachid/WFileServer",
-            image: "wfs.png",
-            tags: ["JavaScript", "NodeJS"]
-        },
-        {
-            id: 3,
-            name: "FixQuote",
-            description: "Equalize/Differentiate quotation marks",
-            url: "https://noorwachid.github.io/Jar/FixQuote",
-            image: "fixquote.png",
-            tags: ["JavaScript"]
-        },
-        {
-            id: 4,
-            name: "Sprint",
-            description: "Speed reader: Read article or anything faster than ever",
-            url: "https://noorwachid.github.io/Jar/Sprint",
-            image: "sprint.png",
-            tags: ["JavaScript"]
-        },
-        {
-            id: 5,
-            name: "AnggurEngine",
-            description: "Another 2D C++ Game Engine",
-            url: "https://github.com/NoorWachid/Anggur",
-            image: "anggur.png",
-            tags: ["C++"]
-        },
-        {
-            id: 5,
-            name: "SimpleBlog",
-            description: "Super simple blog written in PHP with MySQL",
-            url: "https://github.com/NoorWachid/SimpleBlog",
-            image: "blank.png",
-            tags: ["PHP", "MySQL"]
-        }
-    ]
-};
-
 let data = data_EN;
 
 function createProjectNodeTag(id, name) {
@@ -176,6 +80,8 @@ function updateProjectList() {
 function initUiLanguage() {
     $("#dev-name").text(data.dev.name);
     $("#dev-description").text(data.dev.description);
+    $("#dev-email").text(data.dev.email);
+    $("#dev-phone").text(data.dev.phone);
     $("#ui-cv").text(data.ui.cv);
     $("#ui-projects-h").text(data.ui.projects);
     $("#ui-projects").text(data.ui.projects);
@@ -188,6 +94,23 @@ function initUiLanguage() {
     $("#ui-email").text(data.ui.email);
     $("#ui-phone").text(data.ui.phone);
     $("#ui-copyright").text(data.ui.copyright);
+
+    let mainSkillHtml = '';
+    for (let i = 0; i < data.dev.mainSkills.length; ++i) {
+        const skill = data.dev.mainSkills[i];
+        mainSkillHtml += `<div>
+            <h5 class="mb-0">${skill.name}</h5>
+            <p class="text-secondary">${skill.description}</p>
+        </div>`;
+    }
+    $("#dev-main-skills").html(mainSkillHtml);
+
+    let otherSkillHtml = '';
+    for (let i = 0; i < data.dev.otherSkills.length; ++i) {
+        const skill = data.dev.otherSkills[i];
+        otherSkillHtml += `<p>${skill}</p>`;
+    }
+    $("#dev-other-skills").html(otherSkillHtml);
 }
 
 $(document).ready(function () {
